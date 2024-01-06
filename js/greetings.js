015bf186aa9efe77c20a84f5f73dcf87b1d20c66
ljh8450 <dzigh30@gmail.com>
2024-01-06 19:46:19 +0900

    Change code

--- js/greetings.js
+++ js/greetings.js
@@ -11,7 +11,7 @@ function onLoginSubmit(event){
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
-    return false;
+    
 }
 
 function paintGreetings(username) {
