95899271413172e5d2db04e004eb4d3e88305ee2
ljh8450 <dzigh30@gmail.com>
2024-01-30 23:50:24 +0900

    Cool!
    Set Date and CSS

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("h2#clock");
+const myDate = document.querySelector("h1#myDate");
 
 function getClock() {
     const date = new Date();
@@ -8,5 +9,34 @@ function getClock() {
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+function reternDow(dOW){
+    if (dOW === 0) {
+        return "SUN"
+    }else if (dOW === 1) {
+        return "MON"
+    }else if (dOW === 2) {
+        return "TUE"
+    }else if (dOW === 3) {
+        return "WED"
+    }else if (dOW === 4) {
+        return "THU"
+    }else if (dOW === 5) {
+        return "FRI"
+    }else {
+        return "SAT"
+    }
+}
+
+function getDate() {
+    const date = new Date();
+    const years = String(date.getFullYear()).padStart(4, "0");
+    const months = String(date.getMonth()+1).padStart(2, "0");
+    const days = String(date.getDate()).padStart(2, "0");
+    const dOW = reternDow(new Date(`${years}-${months}-${days}`).getDay());
+    myDate.innerText = `${years}/${months}/${days} ${dOW}`
+}
+
 getClock();
 setInterval(getClock, 1000);
+getDate();
+setInterval(getDate, 24*60*60*1000);
